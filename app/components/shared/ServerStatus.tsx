032c5b6076a2f316144293ccd5af05ef66f4995a
george <george@George-Ubuntu-24.04.1-LTS>
2024-12-24 14:52:10 +0000

    Refined offline page

--- app/components/shared/ServerStatus.tsx
+++ app/components/shared/ServerStatus.tsx
@@ -2,7 +2,6 @@
 import { useState, useEffect } from "react";
 import BackgroundWrapper from "./BackgroundWrapper";
 import { motion } from 'framer-motion';
-import Image from "next/image";
 
 export default function ServerStatus({ children }: { children: React.ReactNode }) {
     const [isOnline, setIsOnline] = useState<boolean | null>(null);
